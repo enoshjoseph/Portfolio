81e422df122415584d2c968ccbe2d19e35c67003
enoshjoseph <enoshjoseph1@gmail.com>
2025-12-22 22:38:15 +0530

    fix section on portfolio.jsx

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,6 +1,5 @@
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
-import Section from "../components/Section";
 import Skills from "../components/Skills";
 import Projects from "../components/Projects";
 import Achievements from "../components/Achievements";
